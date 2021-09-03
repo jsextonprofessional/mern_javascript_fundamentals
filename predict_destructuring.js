d1847f319e95340447c9dfada9d81d5ab45697f6
jsprofessional <jshuman@protonmail.com>
2021-09-03 16:35:06 -0500

    Completed predict_destructuring.js

--- predict_destructuring.js
+++ predict_destructuring.js
@@ -1,4 +1,4 @@
-// 1.
+// 1. DONE
 // given:
 
 // const cars = ['Tesla', 'Mercedes', 'Honda']
@@ -12,25 +12,26 @@
 // result: logs 'tesla', 'mercedes'
 
 
-// 2.
+// 2. DONE
 // given:
 
-const employee = {
-    name: 'Elon',
-    age: 47,
-    company: 'Tesla'
-}
-const { name: otherName } = employee;
-//Predict the output
-console.log(name);
-console.log(otherName);
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// const { name } = employee;
+// console.log(name);
+// console.log(employee);
+// console.log(otherName);
 
 
 // prediction:
 // result:
 
-// // 3.
-// // given:
+// 3.
+// given:
 
 // const person = {
 //     name: 'Phil Smith',
@@ -44,40 +45,46 @@ console.log(otherName);
 // console.log(hashedPassword);
 
 
-// // prediction:
-// // result
+// prediction:
+// result:
 
-// // 4.
-// // given:
+// 4.
+// given:
 
 // const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
 // const [,first] = numbers;
 // const [,,,second] = numbers;
 // const [,,,,,,,,third] = numbers;
 // //Predict the output
+// // does 2 == 5? false
 // console.log(first == second);
+// // does 2 == 2? true
 // console.log(first == third);
+// console.log(numbers);
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// prediction: logs true, false
+// result
+
+// 5.
+// given:
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+// Predict the output
+// prints 'value'
+console.log(key); 
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
 
 
-// // prediction:
-// // result
-
-// // 5.
-// // given:
-
-// const lastTest = {
-//     key: 'value',
-//     secondKey: [1, 5, 1, 8, 3, 3]
-// }
-// const { key } = lastTest;
-// const { secondKey } = lastTest;
-// const [ ,willThisWork] = secondKey;
-// //Predict the output
-// console.log(key);
-// console.log(secondKey);
-// console.log(secondKey[0]);
-// console.log(willThisWork);
-
-
-// // prediction:
-// // result
+// prediction: "value", [1,5,1,8,3,3], 1, 5
+// result: correct
